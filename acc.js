a3d40fd4bcb32ca349fbb28cb44bff35e8320830
Pavel <p.zinovkin@mail.ru>
2020-12-18 08:59:47 +0300

    аккардион работа

---
+++ acc.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('#accordeon .acc-head').on('click', f_acc);
+});
+
+function f_acc(){
+	//$('#accordeon .ac-body').slideUp(1000);
+	$('#accordeon .acc-body').not(    $(this).next()   ).slideUp(1000);
+	$(this).next().slideToggle(1000);
+}
+window.addEventListener('DOMcontentLoaded',function(){
+    document.querySelector('bb').classList.toggle('.span-btn-plus')
+});
