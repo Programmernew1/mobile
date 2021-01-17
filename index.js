b56d6632221ce354cd31113bf98cb3ecf89d453c
Pavel <p.zinovkin@mail.ru>
2021-01-17 21:14:14 +0300

    сделал стрелки и булеты

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ var mySwiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination',
       clickable:true,    /*нажимаешь на слайд - он переключается */ 
       hideOnClick:true,  /* переключение слайда при нажатии на точку*/ 
+      
     },
     
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+     
     },
+    loop:true,  /* бесконечный слайдер*/ 
   
     // And if we need scrollbar
     scrollbar: {
@@ -25,3 +28,9 @@ document.querySelector('.burger-box').onclick =function(){
   document.querySelector('.burger-menu').classList.toggle('burger-menu-active')
 }; 
 
+document.querySelectorAll('span.swiper-pagination-bullet').forEach (function(ff){
+  ff.style.background='none'
+}); 
+document.querySelectorAll('span.swiper-pagination-bullet').forEach(function(ff2){
+ff2.style.background='white'
+});
